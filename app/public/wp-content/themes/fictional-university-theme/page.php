54b0d3019e5677779328d7dc4b8b17f3f2adfba4
Simas <simonas.deimantavicius@mediapark.com>
2019-07-17 09:28:33 +0300

    add dynamyc rendering of breadcrumb trail

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -15,19 +15,26 @@ while (have_posts()) {
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+    <?php
+    $parentPageId = wp_get_post_parent_id(get_the_ID());
+    if ($parentPageId) {
+      ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPageId) ?>">
+            <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentPageId); ?>
+          </a> <span class="metabox__main"><?php the_title() ?></span></p>
+      </div>
+    <?php } ?>
+
+
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <ul class="min-list">
+        <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li>
+      </ul>
     </div>
 
-    <!--
-            <div class="page-links">
-              <h2 class="page-links__title"><a href="#">About Us</a></h2>
-              <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-              </ul>
-            </div>
-        -->
 
     <div class="generic-content">
       <?php the_content(); ?>
