e735bc6c845022385dca6e7c38d1b5864ec2e53f
Simas <simonas.deimantavicius@mediapark.com>
2019-07-24 16:07:58 +0300

    nav link dynamic classes  based on post type

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -83,7 +83,7 @@
       ?>
 
       <p class="t-center no-margin">
-        <a href="#" class="btn btn--yellow">View All Blog Posts</a>
+        <a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a>
       </p>
     </div>
   </div>
