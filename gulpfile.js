3b07741d442ef70a7631dafbb62713d3f4a2066c
Simas <simonas.deimantavicius@mediapark.com>
2019-07-25 09:03:45 +0300

    finalize Gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 var gulp = require('gulp'),
-settings = require('./settings'),
-webpack = require('webpack'),
-browserSync = require('browser-sync').create(),
-postcss = require('gulp-postcss'),
-rgba = require('postcss-hexrgba'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins'),
-colorFunctions = require('postcss-color-function');
+  settings = require('./settings'),
+  webpack = require('webpack'),
+  browserSync = require('browser-sync').create(),
+  postcss = require('gulp-postcss'),
+  rgba = require('postcss-hexrgba'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import'),
+  mixins = require('postcss-mixins'),
+  colorFunctions = require('postcss-color-function');
 
 gulp.task('styles', function() {
-  return gulp.src(settings.themeLocation + 'css/style.css')
+  return gulp
+    .src(settings.themeLocation + 'css/style.css')
     .pipe(postcss([cssImport, mixins, cssvars, nested, rgba, colorFunctions, autoprefixer]))
-    .on('error', (error) => console.log(error.toString()))
+    .on('error', error => console.log(error.toString()))
     .pipe(gulp.dest(settings.themeLocation));
 });
 
@@ -29,26 +30,36 @@ gulp.task('scripts', function(callback) {
   });
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function(done) {
   browserSync.init({
     notify: false,
     proxy: settings.urlToPreview,
-    ghostMode: false
+    ghostMode: false,
   });
 
-  gulp.watch('./**/*.php', function() {
+  gulp.watch('./**/*.php', function(done) {
     browserSync.reload();
+    done();
   });
+
   gulp.watch(settings.themeLocation + 'css/**/*.css', gulp.parallel('waitForStyles'));
-  gulp.watch([settings.themeLocation + 'js/modules/*.js', settings.themeLocation + 'js/scripts.js'], gulp.parallel('waitForScripts'));
+  gulp.watch(
+    [settings.themeLocation + 'js/modules/*.js', settings.themeLocation + 'js/scripts.js'],
+    gulp.parallel('waitForScripts'),
+  );
 });
 
-gulp.task('waitForStyles', gulp.series('styles', function() {
-  return gulp.src(settings.themeLocation + 'style.css')
-    .pipe(browserSync.stream());
-}))
+gulp.task(
+  'waitForStyles',
+  gulp.series('styles', function() {
+    return gulp.src(settings.themeLocation + 'style.css').pipe(browserSync.stream());
+  }),
+);
 
-gulp.task('waitForScripts', gulp.series('scripts', function(cb) {
-  browserSync.reload();
-  cb()
-}))
+gulp.task(
+  'waitForScripts',
+  gulp.series('scripts', function(cb) {
+    browserSync.reload();
+    cb();
+  }),
+);
